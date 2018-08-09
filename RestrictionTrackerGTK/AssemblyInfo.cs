96c53da32b79b3144ad5f07b606c327becdf96ad
Andy <webmaster@RealityRipple.com>
2018-08-09 11:27:06 -0700

    Update DLL and Increment Version Numbers

--- RestrictionTrackerGTK/AssemblyInfo.cs
+++ RestrictionTrackerGTK/AssemblyInfo.cs
@@ -13,5 +13,5 @@ using System.Runtime.CompilerServices;
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
-[assembly: AssemblyVersion("1.5.3")]
-[assembly: AssemblyFileVersion("1.5.3")]
+[assembly: AssemblyVersion("1.5.4")]
+[assembly: AssemblyFileVersion("1.5.4")]
